b9e5dca989ffa0df2af86962e598540401978d48
aldosch <git@aldo.io>
2024-01-03 12:52:52 +1100

    add web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         {children}
         <SpeedInsights debug={true} />
+        <Analytics debug={true} />
       </body>
     </html>
   );
